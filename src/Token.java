ea3ead42fab79238df0baabae2c0b09d9d5059c6
S3BzA <sebguim2002@gmail.com>
2024-08-15 10:14:31 +0200

    feat: add token class to capture accepted tokens

---
+++ src/Token.java
@@ -0,0 +1,65 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Token {
+    private int id;
+    private String tokenClass;
+    private String word;
+
+    public Token(int id, String tokenClass, String word) {
+        this.id = id;
+        this.tokenClass = tokenClass;
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTokenClass() {
+        return tokenClass;
+    }
+
+    public void setTokenClass(String tokenClass) {
+        this.tokenClass = tokenClass;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String toXML() {
+        return "<TOK>\n" +
+               "  <ID>" + id + "</ID>\n" +
+               "  <CLASS>" + tokenClass + "</CLASS>\n" +
+               "  <WORD>" + word + "</WORD>\n" +
+               "</TOK>";
+    }
+
+	public static Token fromXML(String xml) {
+        Pattern idPattern = Pattern.compile("<ID>(\\d+)</ID>");
+        Pattern classPattern = Pattern.compile("<CLASS>([^<]+)</CLASS>");
+        Pattern wordPattern = Pattern.compile("<WORD>([^<]+)</WORD>");
+
+        Matcher idMatcher = idPattern.matcher(xml);
+        Matcher classMatcher = classPattern.matcher(xml);
+        Matcher wordMatcher = wordPattern.matcher(xml);
+
+        if (idMatcher.find() && classMatcher.find() && wordMatcher.find()) {
+            int id = Integer.parseInt(idMatcher.group(1));
+            String tokenClass = classMatcher.group(1);
+            String word = wordMatcher.group(1);
+            return new Token(id, tokenClass, word);
+        } else {
+            throw new IllegalArgumentException("Invalid XML format");
+        }
+    }
+}
