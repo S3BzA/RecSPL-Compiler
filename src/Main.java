61bd82256821d3d39de394ce1da176a75d252b47
S3BzA <sebguim2002@gmail.com>
2024-08-15 11:21:34 +0200

    feat: update main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		Lexer lexer = new Lexer("SPLProgrammes/Main.txt");
-		lexer.importTokens("tokens.xml");
+		lexer.tokenize();
 		System.out.println(lexer.toString());
 	}
 }
