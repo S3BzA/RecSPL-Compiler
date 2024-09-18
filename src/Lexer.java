738fd1dcef4039cd9fdab9294f568a048ae74163
S3BzA <sebguim2002@gmail.com>
2024-09-18 14:35:38 +0200

    refac; use 1 regex for all matches

--- src/Lexer.java
+++ src/Lexer.java
@@ -8,92 +8,96 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
-	private final SourceReader sourceReader;
-	private final List<Token> tokens = new ArrayList<>();
+    private final SourceReader sourceReader;
+    private final List<Token> tokens = new ArrayList<>();
 
-	Lexer(String sourceFile) {
-		sourceReader = new SourceReader();
-		try {
-			sourceReader.readSourceCode(sourceFile);
-		} catch (Exception e) {
-			System.err.println("Error reading source file: " + e.getMessage());
-		}
-	}
+    Lexer(String sourceFile) {
+        sourceReader = new SourceReader();
+        try {
+            sourceReader.readSourceCode(sourceFile);
+    	} catch (Exception e) {
+            System.err.println("Error reading source file: " + e.getMessage());
+        }
+    }
 
-	public List<Token> getTokens() {
-		return tokens;
-	}
+    public List<Token> getTokens() {
+        return tokens;
+    }
 
-	public void tokenize() { // * This function will use our regex's, @James, to tokenize the source code
-		String line;
-		int tokenCounter = 1;
-		while ((line = sourceReader.nextLine()) != null) {
-			String[] words = line.split("\s+");// split by whitespace
-			for (String word : words) {
-				if (word.equals("")) {
-					continue;
-				}
+    public void tokenize() {
+        List<String> sourceCodeLines = sourceReader.getSourceCode();
+		String sourceCode = String.join("\n", sourceCodeLines);
+		System.out.println("Source code:\n" + sourceCode);
 
-				if (TokenType.isKeyword(word)) {
-					tokens.add(new Token(tokenCounter++, TokenType.fromString(word), word));
-					continue;
-				} else if (isVariableName(word)) {
-					tokens.add(new Token(tokenCounter++, TokenType.V , word));
-					continue;
-				} else if (isFunctionName(word)) {
-					tokens.add(new Token(tokenCounter++, TokenType.F, word));
-					continue;
-				}
-			}
-		}
-		exportTokens();
-	}
+        String regex = "\\b(main|begin|end|skip|halt|print|< input|num|text|void|if|then|else|not|sqrt|or|and|eq|grt|add|sub|mul|div|=|(|)|,|{|})\\b|V_[a-z]([a-z]|[0-9])*|F_[a-z]([a-z]|[0-9])*\\(";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(sourceCode);
+        int tokenCounter = 1;
 
-	private boolean isVariableName(String word) {
-		return word.matches("V_[a-z]([a-z]|[0-9])*");
-	}
+        while (matcher.find()) {
+            String match = matcher.group();
+            TokenType type;
 
-	private boolean isFunctionName(String word) {
-		return word.matches("F_[a-z]([a-z]|[0-9])*\\(");
-	}
+            if (TokenType.isKeyword(match)) {
+                type = TokenType.fromString(match);
+            } else if (isVariableName(match)) {
+                type = TokenType.V;
+            } else if (isFunctionName(match)) {
+                type = TokenType.F;
+            } else {
+                continue;
+            }
 
-	private void exportTokens() { // save tokens to a file.xml
-		StringBuilder xml = new StringBuilder();
-		xml.append("<TOKENS>\n");
-		for (Token token : tokens) {
-			xml.append(token.toXML()).append("\n");
-		}
-		xml.append("</TOKENS>");
-		try {
-			Files.write(Paths.get("tokens.xml"), xml.toString().getBytes());
-		} catch (IOException e) {
-			System.err.println("Error writing tokens to file: " + e.getMessage());
-		}
-	}
+            tokens.add(new Token(tokenCounter++, type, match));
+        }
+        exportTokens();
+    }
 
-	public void importTokens(String file) {
-		try {
-			String content = new String(Files.readAllBytes(new File(file).toPath()));
+    private boolean isVariableName(String word) {
+        return word.matches("V_[a-z]([a-z]|[0-9])*");
+    }
 
-			Pattern tokPattern = Pattern.compile("<TOK>(.*?)</TOK>", Pattern.DOTALL);
-			Matcher matcher = tokPattern.matcher(content);
+    private boolean isFunctionName(String word) {
+        return word.matches("F_[a-z]([a-z]|[0-9])*\\(");
+    }
 
-			while (matcher.find()) {
-				String tokXML = matcher.group(0);
-				Token token = Token.fromXML(tokXML);
-				tokens.add(token);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void exportTokens() { // save tokens to a file.xml
+        StringBuilder xml = new StringBuilder();
+        xml.append("<TOKENS>\n");
+        for (Token token : tokens) {
+            xml.append(token.toXML()).append("\n");
+        }
+        xml.append("</TOKENS>");
+        try {
+            Files.write(Paths.get("tokens.xml"), xml.toString().getBytes());
+        } catch (IOException e) {
+            System.err.println("Error writing tokens to file: " + e.getMessage());
+        }
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder str = new StringBuilder();
-		for (Token token : tokens) {
-			str.append(token.toString()).append("\n");
-		}
-		return str.toString();
-	}
+    public void importTokens(String file) {
+        try {
+            String content = new String(Files.readAllBytes(new File(file).toPath()));
+
+            Pattern tokPattern = Pattern.compile("<TOK>(.*?)</TOK>", Pattern.DOTALL);
+            Matcher matcher = tokPattern.matcher(content);
+
+            while (matcher.find()) {
+                String tokXML = matcher.group(0);
+                Token token = Token.fromXML(tokXML);
+                tokens.add(token);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (Token token : tokens) {
+            str.append(token.toString()).append("\n");
+        }
+        return str.toString();
+    }
 }
